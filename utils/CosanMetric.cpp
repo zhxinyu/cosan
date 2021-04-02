fe73883d53d53e3b23828d1ec72e1975306eadbb
Gracenut <gracechen@brandeis.edu>
2021-04-01 22:44:20 -0400

    build structure for metric

--- utils/CosanMetric.cpp
+++ utils/CosanMetric.cpp
@@ -4,12 +4,25 @@
 // import other libraries
 #include<CosanMetric.h>
 
-
 /*
-Metrics to evaluate performance of a prediction. 
+	Metrics to evaluate performance of a prediction. 
+	TODO: add module
+	TOERR: didn't add namespace
 */
 
-/*
 
-*/
-export double 
+double error_num(CosanMatrix& y_true, CosanMatrix& y_predict){
+	
+};
+
+double mabs_error(CosanMatrix& y_true, CosanMatrix& y_predict){
+
+};
+
+double mse_mean(CosanMatrix& y_true, CosanMatrix& y_predict){
+
+};
+
+double r2_score(CosanMatrix& y_true, CosanMatrix& y_predict){
+
+};
--- utils/CosanMetric.h
+++ utils/CosanMetric.h
@@ -1,11 +1,56 @@
 #ifndef COSAN_METRIC_H
 #define COSAN_METRIC_H
 
-#include<>
+// TODO: change after integrate with module
+#include<string>
+#include<Eigen/Dense>
+#include<cosan/data/CosanData.h>
 
 namespace Cosan
 {
+	/*
+	count the number of errors in a prediction
+	Input:
+		y_true: CosanMatrix; the real labels
+		y-predict: CosanMatrix; the predicted labels
+	Output:
+		result: double; number of mismatch between predicted
+				and real labels
+	*/
+	double error_num(CosanMatrix& y_true, CosanMatrix& y_predict);
 
+	/*
+	Mean absolute error
+	Input:
+		y_true: see in error_num cooments
+		y-predict: see in error_num cooments
+	Output:
+		result: double; refer to 
+				https://scikit-learn.org/stable/modules/model_evaluation.html#mean-absolute-error
+	*/
+	double mabs_error(CosanMatrix& y_true, CosanMatrix& y_predict);
+
+	/*
+	Mean squared error
+	Input:
+		y_true: see in error_num cooments
+		y-predict: see in error_num cooments
+	Output:
+		result: double; refer to
+				https://scikit-learn.org/stable/modules/model_evaluation.html#mean-squared-error
+	*/
+	double mse_mean(CosanMatrix& y_true, CosanMatrix& y_predict);
+
+	/*
+	R2 score, computes the coefficient of determination
+	Input:
+		y_true: see in error_num cooments
+		y-predict: see in error_num cooments
+	Output:
+		result: double; refer to
+				https://scikit-learn.org/stable/modules/model_evaluation.html#r2-score-the-coefficient-of-determination
+	*/
+	double r2_score(CosanMatrix& y_true, CosanMatrix& y_predict);
 
 }
 
